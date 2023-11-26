ae1735a8dc9775973852b036e359e4d3bed26212
farhanshahriyar <farhan.abir9999@gmail.com>
2023-11-26 14:37:55 +0600

    added mui and tailwind and also organized files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <h1 className="text-xl font-bold underline bg-black text-white">
+      Hello world!
+    </h1>
+
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+    </>
+  )
+}
+
+export default App
