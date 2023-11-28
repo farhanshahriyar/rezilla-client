8fd9dd42e5b1fa1f806aa73c08832367f9073ecd
farhanshahriyar <farhan.abir9999@gmail.com>
2023-11-28 22:38:11 +0600

    added agent role and minor problem solved

--- src/hooks/useAgent.jsx
+++ src/hooks/useAgent.jsx
@@ -9,7 +9,7 @@ const useAgent = () => {
     const { data: isAgent } = useQuery({
       queryKey: ["isAgent", user?.email],
       queryFn: async () => {
-        const res = await axiosSecure.length(`/users/agent/${user.email}`);
+        const res = await axiosSecure.get(`/users/agent/${user.email}`);
         console.log(res.data?.agent);
         return res.data?.agent;
       } 
